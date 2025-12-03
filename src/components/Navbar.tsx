3305a965edc7f0b7fcc326605c3dd33f4587fd03
Ben Siri <ben@acsiri.com>
2025-12-03 15:00:02 -0500

    formating

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { observer } from 'mobx-react-lite';
+import Spotify from '../util/spotify';
+
+const Navbar: React.FC = observer(() => {
+    return (
+        <div>
+            <p>nav</p>
+        </div>
+    );
+});
+
+export default Navbar;
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,10 +3,12 @@ import { observer } from 'mobx-react-lite';
 import Spotify from '../util/spotify';
 
 const Sidebar: React.FC = observer(() => {
-    return <div>
-        <h1>Spotify Dashboard</h1>
-        <p>This is a placeholder for the Dashboard page.</p>
-    </div>
+    return(
+        <div>
+            <h1>Spotify Dashboard</h1>
+            <p>This is a placeholder for the Dashboard page.</p>
+        </div>
+    );
 });
 
 export default Sidebar;
